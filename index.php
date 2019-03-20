a39bee677adac2473d129e91da6eb5a45d3a662d
dominicho97 <dominic.ho97@gmail.com>
2019-03-20 10:25:25 +0100

    added layout header/footer, connection error check (5)

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 //connect to db
-$conn = mysqli_connect('localhost', 'dominic','test1234', 'ninja_pizza')
+$conn = mysqli_connect('localhost', 'dominic','test1234', 'ninja_pizza');
 
+//check connection
+if (!$conn){
+  echo'Connection error '.mysqli_connect_error();
 
+}
 
 
 ?>
