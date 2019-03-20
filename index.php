3d6e2943035cbed34c679253f0aa20af1314b5ee
dominicho97 <dominic.ho97@gmail.com>
2019-03-20 10:05:18 +0100

    connected to database (3)

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-//mysql or PDO
+//connect to db
+$conn = mysqli_connect('localhost', 'dominic','test1234', 'ninja_pizza')
 
 
 
@@ -9,6 +10,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php include('templates/header.php');?>
 
-
+<?php include('templates/footer.php');?>
 </html>
