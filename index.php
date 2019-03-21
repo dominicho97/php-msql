43127d4f726fc790c46915e02674797766d7b197
dominicho97 <dominic.ho97@gmail.com>
2019-03-21 09:45:27 +0100

    added order by (12)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (!$conn){
 }
 //write query for al pizzas
 
-$sql = 'SELECT title,ingredients, id FROM pizzas';
+$sql = 'SELECT title,ingredients, id FROM pizzas ORDER BY created_at';
 
 //make query and get results
 
@@ -17,6 +17,16 @@ $result = mysqli_query($conn, $sql);
 
 //fetch the resukting rows as an array
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+//---------optional/goodpractice
+//free result from memory
+mysqli_free_result($result);
+
+//close connection
+mysqli_close($conn);
+//----------optional/good practice
+
 
 print_r($pizzas);
 
