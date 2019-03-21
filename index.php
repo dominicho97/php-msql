891d78cf8359681f618ade5e689335f953020a5c
dominicho97 <dominic.ho97@gmail.com>
2019-03-21 10:18:26 +0100

    cleaned up code with source code

--- index.php
+++ index.php
@@ -40,35 +40,28 @@ mysqli_close($conn);
 
 <h4 class="center grey-text">Pizzas!</h4>
 
- <div class="container">
-    <div class="row">
-      <?php foreach($pizzas as $pizza){?>
+	<div class="container">
+		<div class="row">
 
-      <div class="col s6 m3 ">
-        <div class="card z-depth-0">
-          <div class="card-content center">
-            <h6><?php echo htmlspecialchars($pizza['title']);   ?></h6>
-           <div> <?php echo htmlspecialchars($pizza['ingredients']);?></div>
-        </div>
-        <div class="card-acion right-allign">
-          <a href="#" class="brand-text">more info</a>
-         </div>
-      </div>
-    </div>
+			<?php foreach($pizzas as $pizza){ ?>
 
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
 
-        <?php
-        
-        
-      }?>
-      
+			<?php } ?>
 
+		</div>
+	</div>
 
+	<?php include('templates/footer.php'); ?>
 
-?>
-    </div>
-</div>
-
-
-<?php include('templates/footer.php');?>
 </html>
