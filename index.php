f1fa21323a73439839eaac26ec5c3a4c4069cb6b
dominicho97 <dominic.ho97@gmail.com>
2019-03-21 10:14:02 +0100

    added browser code 45-59

--- index.php
+++ index.php
@@ -39,5 +39,38 @@ print_r($pizzas);
 
 <?php include('templates/header.php');?>
 
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+ <div class="container">
+    <div class="row">
+      <?php foreach($pizzas as $pizza){?>
+
+      <div class="col s6 m3 ">
+        <div class="card z-depth-0">
+          <div class="card-content center">
+            <h6><?php echo htmlspecialchars($pizza['title']);   ?></h6>
+           <div> <?php echo htmlspecialchars($pizza['ingredients']);?></div>
+        </div>
+        <div class="card-acion right-allign">
+        <a href="#" class="brand-text">more info</a>
+         </div>
+      </div>
+    </div>
+
+
+        <?php
+        
+        
+      }?>
+      
+
+
+
+?>
+    </div>
+</div>
+
+
 <?php include('templates/footer.php');?>
 </html>
