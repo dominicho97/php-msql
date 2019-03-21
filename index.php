ccf171eb8cd4bfe809de500451a2899b83a0b336
dominicho97 <dominic.ho97@gmail.com>
2019-03-21 12:11:06 +0100

    added add file and code

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-//connect to db
-$conn = mysqli_connect('localhost', 'dominic','test1234', 'ninja_pizza');
 
-//check connection
-if (!$conn){
-  echo'Connection error '.mysqli_connect_error();
 
-}
+include ('config/db_connect.php');
+
+
 //write query for al pizzas
 
 $sql = 'SELECT title,ingredients, id FROM pizzas ORDER BY created_at';
